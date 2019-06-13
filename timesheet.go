9f0e862abfdf5a88cbd3562f80b3563cce497dbb
Makoto <makoto@emurasoft.com>
2019-06-13 02:02:10 -0700

    .

--- timesheet.go
+++ timesheet.go
@@ -1,15 +1,49 @@
-package timesheet
+package main
 
-import "github.com/getlantern/systray"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+const dataFilepath = "/home/makoto/Documents/timesheet/data"
 
 func main() {
-	systray.Run(onReady, onExit)
+	flag.Parse()
+	//runCommand(flag.Arg(0))
+	if err := runCommand("start"); err != nil {
+		panic(fmt.Sprintf("%+v\n", err))
+	}
 }
 
-func onReady() {
+func runCommand(command string) error {
+	switch command {
+	case "":
+	case "start":
+		return writeTime()
+	case "stop":
+	}
 
+	flag.PrintDefaults()
+	return nil
 }
 
-func onExit() {
+func writeTime() error {
+	b, err := time.Now().MarshalText()
+	if err != nil {
+		return err
+	}
 
+	if err := os.Mkdir(dataFilepath, os.ModePerm); err != nil && os.IsNotExist(err) {
+		return err
+	}
+
+	file, err := os.OpenFile(dataFilepath+"/startTime", os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(b)
+	return err
 }
